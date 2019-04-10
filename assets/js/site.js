e19f6131e6c065498d5b00bba4e9804edbb0377e
jgvorbec <jgvorbec@us.ibm.com>
2019-04-10 17:11:42 -0400

    more content and features

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,28 +1,60 @@
-let buttons = document.querySelectorAll(".album-list__button");
+let Buttons = document.querySelectorAll(".album-list__button");
 const logo = '<svg xmlns="http://www.w3.org/2000/svg" height="512" viewBox="0 -144 512 512" width="512"><path d="m400 224h-272c-8.8 0-16-7.2-16-16s7.2-16 16-16h272c8.8 0 16 7.2 16 16s-7.2 16-16 16zm0 0"/><path d="m112 224c-61.8 0-112-50.2-112-112s50.2-112 112-112 112 50.3 112 112-50.2 112-112 112zm0-192c-44.1 0-80 35.9-80 80s35.9 80 80 80 80-35.9 80-80-35.9-80-80-80zm0 0"/><path d="m400 224c-61.8 0-112-50.2-112-112s50.2-112 112-112 112 50.3 112 112-50.2 112-112 112zm0-192c-44.1 0-80 35.9-80 80s35.9 80 80 80 80-35.9 80-80-35.9-80-80-80zm0 0"/></svg>';
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function() {
-    let ThisProjector   = this.parentElement.parentElement.parentElement.querySelector('.year-block__spotlight'),
-        thisPosition    = (100 / (10 - 1)) * this.dataset.position,
-        ProjectorSprite = document.createElement("DIV"),
-        ProjectorInfo   = document.createElement("P"),
-        artistTextNode  = document.createTextNode(" by " + this.dataset.artist),
-        ProjectorAlbum  = document.createElement("SPAN"),
-        albumTextNode   = document.createTextNode(this.dataset.album);
+for (let i = 0; i < Buttons.length; i++) {
+    // Add event listener to album lists.
+    Buttons[i].addEventListener("click", function() {
+            // Album projector element for the clicked album
+        let ThisProjector        = this.parentElement.parentElement.parentElement.querySelector('.year-block__spotlight'),
+            // Album cover image
+            ProjectorSprite      = document.createElement("DIV"),
+            // Album info container
+            ProjectorAlbumInfo   = document.createElement("P"),
+            // Album name container
+            ProjectorAlbumName   = document.createElement("SPAN"),
+            // Position of album cover in sprite
+            thisPosition         = (100 / (10 - 1)) * this.dataset.position,
+            // Album artist name text
+            artistTextNode       = document.createTextNode(" by " + this.dataset.artist),
+            // Album name text
+            albumNameTextNode    = document.createTextNode(this.dataset.album);
 
-    ProjectorSprite.setAttribute("class", "sprite-slice");
-    ProjectorSprite.setAttribute("style", "background-image:" + this.style["backgroundImage"] + ";background-position: 0% " + thisPosition + "%;");
+        // Add class to image container
+        ProjectorSprite.setAttribute("class", "sprite-slice");
+        // Add class to album info container
+        ProjectorAlbumInfo.setAttribute("class", "sprite-info");
 
-    ProjectorAlbum.appendChild(albumTextNode);
-    ProjectorInfo.setAttribute("class", "sprite-info");
-    ProjectorInfo.appendChild(ProjectorAlbum);
-    ProjectorInfo.appendChild(artistTextNode);
+        // Set album cover as bg-image on container
+        ProjectorSprite.setAttribute("style", "background-image:" + this.style["backgroundImage"] + ";background-position: 0% " + thisPosition + "%;");
 
-    ThisProjector.innerHTML = logo;
-    ThisProjector.appendChild(ProjectorSprite);
-    ThisProjector.appendChild(ProjectorInfo);
+        // If Spotify link exists
+        if (this.dataset.spotify) {
+            let SpotifyLink = document.createElement("A");
+            SpotifyLink.setAttribute("class", "sprite-spotify");
+            SpotifyLink.setAttribute("title", "Listen to " + albumNameTextNode + " on Spotify");
+            SpotifyLink.setAttribute("href", this.dataset.spotify);
+            ProjectorSprite.appendChild(SpotifyLink);
+        }
 
-    console.log(ProjectorSprite);
-  });
+        // Add album name text to container
+        ProjectorAlbumName.appendChild(albumNameTextNode);
+        // Add album name container to album info container
+        ProjectorAlbumInfo.appendChild(ProjectorAlbumName);
+        // Add album artist name text to album info container
+        ProjectorAlbumInfo.appendChild(artistTextNode);
+
+        // Clear projector and replace the logo icon
+        ThisProjector.innerHTML = logo;
+        // Add album info container to the projector
+        ThisProjector.appendChild(ProjectorAlbumInfo);
+        // Add album cover image to the projector
+        ThisProjector.appendChild(ProjectorSprite);
+
+        // Remove/Add "active" class on selected album for the relevant year
+        let ThisYearsAlbums = this.parentElement.parentElement.querySelectorAll(".album-list__button");
+        for (let i = 0; i < ThisYearsAlbums.length; i++) {
+            ThisYearsAlbums[i].parentElement.classList.remove("album-list__item--active");
+        }
+        this.parentElement.classList.add("album-list__item--active")
+    });
 }
