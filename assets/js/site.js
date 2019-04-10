abad94234c06cfda4bb79fcdef88d7a0a2ee7fb2
gvorbeck <gvorbeck@gmail.com>
2019-04-09 23:56:07 -0400

    css reorg for redesign

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,6 @@
 let buttons = document.querySelectorAll(".album-list__button");
+const logo = '<svg xmlns="http://www.w3.org/2000/svg" height="512" viewBox="0 -144 512 512" width="512"><path d="m400 224h-272c-8.8 0-16-7.2-16-16s7.2-16 16-16h272c8.8 0 16 7.2 16 16s-7.2 16-16 16zm0 0"/><path d="m112 224c-61.8 0-112-50.2-112-112s50.2-112 112-112 112 50.3 112 112-50.2 112-112 112zm0-192c-44.1 0-80 35.9-80 80s35.9 80 80 80 80-35.9 80-80-35.9-80-80-80zm0 0"/><path d="m400 224c-61.8 0-112-50.2-112-112s50.2-112 112-112 112 50.3 112 112-50.2 112-112 112zm0-192c-44.1 0-80 35.9-80 80s35.9 80 80 80 80-35.9 80-80-35.9-80-80-80zm0 0"/></svg>';
+
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function() {
     let ThisProjector   = this.parentElement.parentElement.parentElement.querySelector('.year-block__spotlight'),
@@ -17,7 +19,7 @@ for (let i = 0; i < buttons.length; i++) {
     ProjectorInfo.appendChild(ProjectorAlbum);
     ProjectorInfo.appendChild(artistTextNode);
 
-    ThisProjector.innerHTML = "<span class=\"site-logo\"></span>";
+    ThisProjector.innerHTML = logo;
     ThisProjector.appendChild(ProjectorSprite);
     ThisProjector.appendChild(ProjectorInfo);
 
