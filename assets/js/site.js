3361578d0883d67bfc2bc217b5cb248fe8e8bd54
gvorbeck <gvorbeck@gmail.com>
2019-04-11 09:21:44 -0400

    rolled own lazy load solution

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,32 @@
+// SPRITE LAZY LOAD
+function isVisible(elem) {
+    let coords = elem.getBoundingClientRect();
+    let windowHeight = document.documentElement.clientHeight;
+    // top elem edge is visible OR bottom elem edge is visible
+    let topVisible = coords.top > 0 && coords.top < windowHeight;
+    let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
+    return topVisible || bottomVisible;
+}
+function showVisible() {
+    for (let img of document.querySelectorAll(".favorites-list__item button")) {
+        let realSrc = img.dataset.src;
+        if (!realSrc) continue;
+
+        if (isVisible(img.parentElement.parentElement.parentElement.parentElement)) {
+            // disable caching
+            // this line should be removed in production code
+            // realSrc += '?nocache=' + Math.random();
+
+            img.setAttribute("style", "background-image: url(" + realSrc + ");" + img.getAttribute("style"));
+
+            img.dataset.src = '';
+        }
+    }
+}
+window.addEventListener('scroll', showVisible);
+showVisible();
+
+// BUTTON BEHAVIOR
 let Buttons = document.querySelectorAll(".album-list__button");
 const logo = '<svg xmlns="http://www.w3.org/2000/svg" height="512" viewBox="0 -144 512 512" width="512"><path d="m400 224h-272c-8.8 0-16-7.2-16-16s7.2-16 16-16h272c8.8 0 16 7.2 16 16s-7.2 16-16 16zm0 0"/><path d="m112 224c-61.8 0-112-50.2-112-112s50.2-112 112-112 112 50.3 112 112-50.2 112-112 112zm0-192c-44.1 0-80 35.9-80 80s35.9 80 80 80 80-35.9 80-80-35.9-80-80-80zm0 0"/><path d="m400 224c-61.8 0-112-50.2-112-112s50.2-112 112-112 112 50.3 112 112-50.2 112-112 112zm0-192c-44.1 0-80 35.9-80 80s35.9 80 80 80 80-35.9 80-80-35.9-80-80-80zm0 0"/></svg>';
 
