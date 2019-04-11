07b92586e65d0d65f05cfd3d2026bf4ba5cb51d0
gvorbeck <gvorbeck@gmail.com>
2019-04-11 12:57:23 -0400

    redeisgn fixes

--- assets/js/site.js
+++ assets/js/site.js
@@ -33,57 +33,60 @@ const logo = '<svg xmlns="http://www.w3.org/2000/svg" height="512" viewBox="0 -1
 for (let i = 0; i < Buttons.length; i++) {
     // Add event listener to album lists.
     Buttons[i].addEventListener("click", function() {
-            // Album projector element for the clicked album
-        let ThisProjector        = this.parentElement.parentElement.parentElement.querySelector('.year-block__spotlight'),
-            // Album cover image
-            ProjectorSprite      = document.createElement("DIV"),
-            // Album info container
-            ProjectorAlbumInfo   = document.createElement("P"),
-            // Album name container
-            ProjectorAlbumName   = document.createElement("SPAN"),
-            // Position of album cover in sprite
-            thisPosition         = (100 / (10 - 1)) * this.dataset.position,
-            // Album artist name text
-            artistTextNode       = document.createTextNode(" by " + this.dataset.artist),
-            // Album name text
-            albumNameTextNode    = document.createTextNode(this.dataset.album);
+        if (!this.parentElement.classList.contains("album-list__item--active")) {
 
-        // Add class to image container
-        ProjectorSprite.setAttribute("class", "sprite-slice");
-        // Add class to album info container
-        ProjectorAlbumInfo.setAttribute("class", "sprite-info");
+            // Remove/Add "active" class on selected album for the relevant year
+            let ThisYearsAlbums = this.parentElement.parentElement.querySelectorAll(".album-list__button");
+            for (let i = 0; i < ThisYearsAlbums.length; i++) {
+                ThisYearsAlbums[i].parentElement.classList.remove("album-list__item--active");
+            }
+            this.parentElement.classList.add("album-list__item--active");
+            
+                // Album projector element for the clicked album
+            let ThisProjector        = this.parentElement.parentElement.parentElement.querySelector('.year-block__spotlight'),
+                // Album cover image
+                ProjectorSprite      = document.createElement("DIV"),
+                // Album info container
+                ProjectorAlbumInfo   = document.createElement("H4"),
+                // Album name container
+                ProjectorAlbumName   = document.createElement("SPAN"),
+                // Position of album cover in sprite
+                thisPosition         = (100 / (10 - 1)) * this.dataset.position,
+                // Album artist name text
+                artistTextNode       = document.createTextNode(" by " + this.dataset.artist),
+                // Album name text
+                albumNameTextNode    = document.createTextNode(this.dataset.album);
 
-        // Set album cover as bg-image on container
-        ProjectorSprite.setAttribute("style", "background-image:" + this.style["backgroundImage"] + ";background-position: 0% " + thisPosition + "%;");
+            // Add class to image container
+            ProjectorSprite.setAttribute("class", "sprite-slice");
+            // Add class to album info container
+            ProjectorAlbumInfo.setAttribute("class", "sprite-info");
 
-        // If Spotify link exists
-        if (this.dataset.spotify) {
-            let SpotifyLink = document.createElement("A");
-            SpotifyLink.setAttribute("class", "sprite-spotify");
-            SpotifyLink.setAttribute("title", "Listen to " + albumNameTextNode + " on Spotify");
-            SpotifyLink.setAttribute("href", this.dataset.spotify);
-            ProjectorSprite.appendChild(SpotifyLink);
+            // Set album cover as bg-image on container
+            ProjectorSprite.setAttribute("style", "background-image:" + this.style["backgroundImage"] + ";background-position: 0% " + thisPosition + "%;");
+
+            // If Spotify link exists
+            if (this.dataset.spotify) {
+                let SpotifyLink = document.createElement("A");
+                SpotifyLink.setAttribute("class", "sprite-spotify");
+                SpotifyLink.setAttribute("title", "Listen to " + albumNameTextNode + " on Spotify");
+                SpotifyLink.setAttribute("href", this.dataset.spotify);
+                ProjectorSprite.appendChild(SpotifyLink);
+            }
+
+            // Add album name text to container
+            ProjectorAlbumName.appendChild(albumNameTextNode);
+            // Add album name container to album info container
+            ProjectorAlbumInfo.appendChild(ProjectorAlbumName);
+            // Add album artist name text to album info container
+            ProjectorAlbumInfo.appendChild(artistTextNode);
+
+            // Clear projector and replace the logo icon
+            ThisProjector.innerHTML = logo;
+            // Add album info container to the projector
+            ThisProjector.appendChild(ProjectorAlbumInfo);
+            // Add album cover image to the projector
+            ThisProjector.appendChild(ProjectorSprite);
         }
-
-        // Add album name text to container
-        ProjectorAlbumName.appendChild(albumNameTextNode);
-        // Add album name container to album info container
-        ProjectorAlbumInfo.appendChild(ProjectorAlbumName);
-        // Add album artist name text to album info container
-        ProjectorAlbumInfo.appendChild(artistTextNode);
-
-        // Clear projector and replace the logo icon
-        ThisProjector.innerHTML = logo;
-        // Add album info container to the projector
-        ThisProjector.appendChild(ProjectorAlbumInfo);
-        // Add album cover image to the projector
-        ThisProjector.appendChild(ProjectorSprite);
-
-        // Remove/Add "active" class on selected album for the relevant year
-        let ThisYearsAlbums = this.parentElement.parentElement.querySelectorAll(".album-list__button");
-        for (let i = 0; i < ThisYearsAlbums.length; i++) {
-            ThisYearsAlbums[i].parentElement.classList.remove("album-list__item--active");
-        }
-        this.parentElement.classList.add("album-list__item--active")
     });
 }
